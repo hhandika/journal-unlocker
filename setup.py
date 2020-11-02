81e4bec11ffa61b21489b6e2a5c78bffab01e76c
Heru Handika <herubiolog@gmail.com>
2020-11-02 16:04:13 -0600

    Add setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='journal-unlocker',
+    version='0.0.1',
+    author= "Heru Handika",
+    author_email = "hhandika.us@gmail.com",
+    include_package_data=True,
+    py_modules=find_packages(),
+    install_requires=[
+        'Click', 'requests',
+    ],
+    entry_points='''
+        [console_scripts]
+        ju=ju.ju:main
+    ''',
+)
