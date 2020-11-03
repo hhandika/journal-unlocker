a64a456629e7c5da7c293e784c93c09f4806d642
Heru Handika <herubiolog@gmail.com>
2020-11-02 19:55:58 -0600

    Add config writer

--- configs/conf_reader.py
+++ configs/conf_reader.py
@@ -1,6 +1,6 @@
 from configparser import SafeConfigParser
 
-def get_lib_address():
+def read_lib_address():
     parser = SafeConfigParser()
     parser.read('configs/settings.conf')
     return parser.get('Settings', 'libs')
---
+++ configs/conf_writer.py
@@ -0,0 +1,23 @@
+import os
+
+class ConfigWriter:
+    def __init__(self, lib_url):
+        self.lib_url = lib_url
+    
+    def _get_root_path(self):
+        return os.path.dirname(os.path.abspath(__file__))
+
+    def _get_settings(self):
+        ROOT_DIR = self._get_root_path()
+        fname = ROOT_DIR + '/' + 'settings.conf'
+        url = 'libs = ' + self.lib_url
+        return fname, url
+
+    def write_settings(self):
+        fname, url = self._get_settings()
+        f = open(fname, 'a')
+        f.write('[Settings]\n')
+        f.write(url)
+        f.close()
+        print('You are set!')
+        print(f"A config file is written in {fname}")
