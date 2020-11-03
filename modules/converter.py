86d7da6cf9d08dbad2f284cbe585aa51589ed45a
Heru Handika <herubiolog@gmail.com>
2020-11-02 18:01:00 -0600

    Refactor code

--- modules/converter.py
+++ modules/converter.py
@@ -1,42 +1,40 @@
-#!/usr/bin/python
+import sys
 
-import re, webbrowser as wb, click
+class LinkConverter:
+    def __init__(self, url, libs):
+        self.url = url
+        self.libs = libs
+    
+    def _convert_com(self):
+        split_url = self.url.split('.com')
+        first_part, second_part = split_url
+        first_part = first_part.replace('.','-')
+        final_link = first_part + '-com.' + self.libs + second_part
+        return final_link
+    
+    def _convert_org(self):
+        split_url = self.url.split('.org')
+        first_part, second_part = split_url
+        first_part = first_part.replace('.','-')
+        final_link = first_part + '-org.' + self.libs + second_part
+        return final_link
 
-#Convert the address to lsu library address. Setup click command line function
-def convert_url(web,libs):
-    '''
-    A function to open paywalled scientific papers using lsu library access. 
-    It takes paywalled journal/article urls or full doi web address. 
-    '''
-    com = '.com'
-    org = '.org'
-    edu = '.edu'
-    if com in web:
-        split_url = web.split('.com')
-        #unpack the url
+    def _convert_edu(self):
+        split_url = self.url.split('.edu')
         first_part, second_part = split_url
-        #Replace dot in the first part to dash
         first_part = first_part.replace('.','-')
-        #Merge all the url components
-        lsulib_address = first_part + '-com.' + libs + second_part
-        return lsulib_address
-    if org in web:
-        split_url = web.split('.org')
-        #unpack the url
-        first_part, second_part = split_url
-        #Replace dot in the first part to dash
-        first_part = first_part.replace('.','-')
-        #Merge all the url components
-        lsulib_address = first_part + '-org.' + libs + second_part
-        return lsulib_address
-    if edu in web:
-        split_url = web.split('.edu')
-        #unpack the url
-        first_part, second_part = split_url
-        #Replace dot in the first part to dash
-        first_part = first_part.replace('.','-')
-        #Merge all the url components
-        lsulib_address = first_part + '-edu.' + libs + second_part
-        return lsulib_address
-    else:
-        print('Sorry... the url is not supported') 
+        final_link = first_part + '-edu.' + self.libs + second_part
+        return final_link
+
+    def convert_url(self):
+        if '.com' in self.url:
+            url = self._convert_com()
+            return url
+        elif '.org' in self.url:
+            url = self._convert_org()
+            return url
+        elif '.edu' in self.url:
+            url = self._convert_edu()
+            return url
+        else:
+            sys.exit(f'\x1b[0;31mERROR: \x1b[0mTHE URL IS NOT SUPPORTED') 
