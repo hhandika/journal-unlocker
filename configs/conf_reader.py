61f1db9026f9d1e26123187ba50b12202e2e0e12
Heru Handika <herubiolog@gmail.com>
2020-11-02 20:25:17 -0600

    Ask user to input settings if it does not exist

--- configs/conf_reader.py
+++ configs/conf_reader.py
@@ -1,6 +1,23 @@
 from configparser import SafeConfigParser
 
-def read_lib_address():
+from configs import conf_writer
+
+def read_lib_url():
     parser = SafeConfigParser()
     parser.read('configs/settings.conf')
     return parser.get('Settings', 'libs')
+
+def write_user_input(conf):
+    conf_write = conf_writer.ConfigWriter(conf)
+    conf_write.write_settings()
+
+def get_lib_url():
+    try:
+        lib_url = read_lib_url()
+        return lib_url
+    except:
+        print('Error reading config file')
+        print('Add config file')
+        conf = input("Enter library address:")
+        write_user_input(conf)
+        exit()
