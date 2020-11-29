09fb190faa240c7cc3fa1a83de9b408305bae3c0
hhandika <herubiolog@gmail.com>
2020-11-28 22:29:29 -0600

    Add command to display version.

--- ju/ju.py
+++ ju/ju.py
@@ -4,10 +4,16 @@ from configs import conf_reader
 from configs import conf_writer
 from modules import input_output
 
+
+def show_version():
+    version = "v0.0.1"
+    print(f"Journal Unlocker {version}")
+
 @click.command()
 @click.option('--url','-u', help='Add a url, doi link, or doi name.')
 @click.option('--set', default=None, help='Set library url.')
-def main(url, set):
+@click.option('--version', default=None, is_flag=True, help='Show version')
+def main(url, set, version):
     """[summary]
 
     Args:
@@ -19,6 +25,8 @@ def main(url, set):
     if set is not None:
         settings = conf_writer.ConfigWriter(set)
         settings.write_settings()
+    elif version is not None:
+        show_version()
     else:
         libs = conf_reader.get_lib_url()
         ui = input_output.UserInput(libs, url)
